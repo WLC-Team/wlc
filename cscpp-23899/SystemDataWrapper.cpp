2732c31c1be2c2016cc82955942a5984ad62e98b
tamalika123 <bhowmick.tm@gmail.com>
2016-08-31 15:30:51 +0530

    Add files via upload

---
+++ cscpp-23899/SystemDataWrapper.cpp
@@ -0,0 +1,31 @@
+namespace HP.System.Data
+{
+	class DataEntry
+	{
+	private:
+		string key;
+		int value;
+		string value;
+		double value;
+		int valuetype;
+
+		DataEntry(string key);
+
+	};
+	class DataRow
+	{
+		public:
+			DataRow()
+			{
+			}
+	};
+	class DataTable
+	{
+
+		public:
+			DataTable()
+			{
+
+			}
+	};
+}
